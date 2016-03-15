f2f1140602a1a8a99bcd035e2bc9b9065216f85e
Eric Templeton <erictempleton1@gmail.com>
2016-03-15 08:07:41 -0400

    adding mongodb and working on cli

--- todo_list.go
+++ todo_list.go
@@ -2,9 +2,26 @@ package main
 
 import (
     "os"
+    "time"
     "github.com/codegangsta/cli"
+    "gopkg.in/mgo.v2"
+    "gopkg.in/mgo.v2/bson"
 )
 
+type Todos struct {
+    Name string
+    Created time.Time
+    Done bool
+}
+
+
 func main() {
-    cli.NewApp().Run(os.Args)
+    app := cli.NewApp()
+    app.Name = "boom"
+    app.Usage = "make an explosive entrance"
+    app.Action = func(c *cli.Context) {
+        println("Hello", c.Args()[0])
+    }
+
+    app.Run(os.Args)
 }
